2b6777d9039f01235005f6f9fe60ff27a0f25b98
Ike Lockett <ike.lockett@anaplan.com>
2020-06-28 17:20:17 +0100

    Fixed docstrings and removed defunct functionality.

--- name_generator.py
+++ name_generator.py
@@ -14,74 +14,44 @@ from itertools import product
 
 
 def init():
+    """Initialise the name generation."""
     try:
         pairs_df = pd.read_pickle("pairs.pkl")
     except FileNotFoundError:
-        pairs_of_letters = list(product(string.ascii_lowercase, string.ascii_lowercase))
+        pairs_of_letters = list(
+            product(string.ascii_lowercase, string.ascii_lowercase))
         random.shuffle(pairs_of_letters)
         pairs_df = DataFrame(pairs_of_letters)
-    
+
         pairs_df.to_pickle("pairs.pkl")
     return pairs_df
 
 
-def now_the_triples():
-    try:
-        triples_df = pd.read_pickle("triples.pkl")
-    except FileNotFoundError:
-        triples_of_letters = list(product(string.ascii_lowercase,
-                                          string.ascii_lowercase,
-                                          string.ascii_lowercase))
-        random.shuffle(triples_of_letters)
-        triples_df = DataFrame(triples_of_letters)
-        triples_df.to_pickle("triples.pkl")
-    return triples_df
-
-
-def initialise_columns(pairs_df, triples_df):
+def initialise_columns(pairs_df):
+    """Initialise the columns of the df."""
     if "accepted" not in pairs_df.columns:
         pairs_df["accepted"] = ""
-
-    # if "accepted" not in triples_df.columns:
-    #     triples_df["accepted"] = ""
-
 
     if "beginning" not in pairs_df.columns:
         pairs_df["beginning"] = ""
         pairs_df.loc[pairs_df["accepted"] == 0, "beginning"] = 0
-        
-    # if "beginning" not in triples_df.columns:
-    #     triples_df["beginning"] = ""
-    #     triples_df.loc[triples_df["accepted"] == 0, "beginning"] = 0
-        
+
     if "non_beginning" not in pairs_df.columns:
         pairs_df["non_beginning"] = ""
         pairs_df.loc[pairs_df["accepted"] == 0, "non_beginning"] = 0
-        
-    # if "non_beginning" not in triples_df.columns:
-    #     triples_df["non_beginning"] = ""
-    #     triples_df.loc[triples_df["accepted"] == 0, "non_beginning"] = 0
-        
+
     if "rating" not in pairs_df.columns:
         pairs_df["rating"] = ""
     pairs_df.loc[pairs_df["accepted"] == 0, "rating"] = 0
-        
-            
+
     if "pair" not in pairs_df.columns:
         pairs_df["pair"] = pairs_df[0] + pairs_df[1]
-        
-    if "pair_1" not in triples_df.columns:
-        triples_df["pair_1"] = triples_df[0] + triples_df[1]
-        
-    if "pair_2" not in triples_df.columns:
-        triples_df["pair_2"] = triples_df[1] + triples_df[2]
-    
-    
-    return pairs_df, triples_df
 
+    return pairs_df
 
 
 def count_remaining(df):
+    """Count how many letter pairs still need to be evaluated."""
     print(pairs_df[pairs_df["accepted"] == ""].shape[0])
     print(pairs_df[pairs_df["beginning"] == ""].shape[0])
     print(pairs_df[pairs_df["non_beginning"] == ""].shape[0])
@@ -89,7 +59,8 @@ def count_remaining(df):
 
 
 def track_vc_counts(v, c, letter):
-    if letter in ["a","e","i","o","u","y"]:
+    """Keep track of how many vowels / consonants have been used in a row."""
+    if letter in ["a", "e", "i", "o", "u", "y"]:
         v += 1
         c = 0
     else:
@@ -98,18 +69,19 @@ def track_vc_counts(v, c, letter):
     return v, c
 
 
-def create_string_unit(df, string_length=3, starting_string="", ):
+def create_string_unit(df, string_length=3, starting_string=""):
+    """Create a unit of text adhering to some prespecified language rules."""
     final_string = starting_string
-    
+
     valid = df[(df["accepted"] == 1) & (df["beginning"] == 1)]
     first_selection_idx = random.choice(valid.index)
     first_selection = df.iloc[first_selection_idx]
     v = 0
     c = 0
     final_string += (first_selection[0])
-    v, c = track_vc_counts(v, c, first_selection[0]) 
+    v, c = track_vc_counts(v, c, first_selection[0])
     final_string += (first_selection[1])
-    v, c = track_vc_counts(v, c, first_selection[1])    
+    v, c = track_vc_counts(v, c, first_selection[1])
     while len(final_string) < string_length:
         valid = df[(df["accepted"] == 1) & (df["non_beginning"] == 1)]
         final_character = final_string[-1]
@@ -136,18 +108,14 @@ def create_string_unit(df, string_length=3, starting_string="", ):
             return final_string
         else:
             return final_string
-        
     return final_string
 
 
-def generate_syllable(length=5, starting_string=""):
-    return 
-
-
 def generate_word(syllables=2, starting_string=""):
+    """Generate a word with a specified number of syllables."""
     pairs_df = pd.read_pickle("pairs.pkl")
     word = ""
-    for i in range(1,syllables+1):
+    for i in range(1, syllables+1):
         length = round(random.gauss(4, 1))
         word += create_string_unit(pairs_df, string_length=length,
                                    starting_string=starting_string)
@@ -155,8 +123,8 @@ def generate_word(syllables=2, starting_string=""):
     return word
 
 
-
 def pattern_selection_algorithm(df, column="accepted"):
+    """Train algorithm with valid / invalid letter pairs."""
     returned_val = False
     while returned_val != "q":
         todo = df[df[column] == ""].index
@@ -168,17 +136,20 @@ def pattern_selection_algorithm(df, column="accepted"):
             df.iloc[selection][column] = returned_val
             df.to_pickle("pairs.pkl")
     return df
-    
+
 
 def test_pair(df_row, column="accepted"):
+    """Test a pair of letters to see if they fit a certain rule."""
     print(df_row[0] + df_row[1])
     inp = ""
-    accepted = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, "q"] if column == "rating" else [0, 1, "q"]
+    accepted = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, "q"]\
+        if column == "rating" else [0, 1, "q"]
     while inp not in accepted:
         if column == "rating":
             query_string = "How valid is this string? (1-10)\n"
         else:
-            query_string = "Does this pattern work within an English syllable? %s (0/1)" % column
+            query_string = ("Does this work in an English syllable? %s (0/1)"
+                            % column)
         inp = input(query_string)
         try:
             inp = int(inp)
@@ -186,34 +157,15 @@ def test_pair(df_row, column="accepted"):
             pass
     return inp
 
-    
+
 if __name__ == "__main__":
     pairs_df = init()
-    triples_df = now_the_triples()
-    pairs_df, triples_df = initialise_columns(pairs_df, triples_df)
-    # triples_df = triples_df.merge(pairs_df.drop([0,1], axis=1),
-    #                               left_on="pair_1", right_on="pair",
-    #                               suffixes=("","_1"))
-    
-    # triples_df = triples_df.merge(pairs_df.drop([0,1], axis=1),
-    #                               left_on="pair_2", right_on="pair",
-    #                               suffixes=("","_2"))
-        
-    # triples_df["accepted"] = triples_df["accepted_1"] * triples_df["accepted_2"]
-    # triples_df["beginning"] = triples_df["beginning_1"] * triples_df["beginning_2"]
-    # triples_df["non_beginning"] = triples_df["non_beginning_1"] * triples_df["non_beginning_2"]
-    
-    # valid_triples = triples_df[triples_df["accepted"]!=0]
-        
-    if pairs_df[pairs_df["accepted"] == ""].shape[0] > 0:        
+    pairs_df = initialise_columns(pairs_df)
+    if pairs_df[pairs_df["accepted"] == ""].shape[0] > 0:
         pairs_df = pattern_selection_algorithm(pairs_df)
-        
-    if pairs_df[pairs_df["beginning"] == ""].shape[0] > 0:        
+    if pairs_df[pairs_df["beginning"] == ""].shape[0] > 0:
         pairs_df = pattern_selection_algorithm(pairs_df, "beginning")
-        
-    if pairs_df[pairs_df["non_beginning"] == ""].shape[0] > 0:        
+    if pairs_df[pairs_df["non_beginning"] == ""].shape[0] > 0:
         pairs_df = pattern_selection_algorithm(pairs_df, "non_beginning")
-        
     if pairs_df[pairs_df["rating"] == ""].shape[0] > 0:
         pairs_df = pattern_selection_algorithm(pairs_df, "rating")
-        
