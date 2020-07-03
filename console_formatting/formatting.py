6a5ef7c17f6565caa9f18abac6f23e80e3c81827
Ike Lockett <ike.lockett@anaplan.com>
2020-07-03 09:11:33 +0100

    light_white instead of white

--- console_formatting/formatting.py
+++ console_formatting/formatting.py
@@ -6,7 +6,7 @@ Created on Tue Jun 30 19:53:34 2020
 @author: ikelockett
 """
 
-from console_formatting.ansi_colours import light_cyan, light_green, light_red, white, yellow
+from console_formatting.ansi_colours import light_cyan, light_green, light_red, light_white, yellow
 
 
 def colour_map_1_to_10(x):
@@ -17,9 +17,9 @@ def colour_map_1_to_10(x):
         2: light_red,
         3: yellow,
         4: yellow,
-        5: white,
-        6: white,
-        7: white,
+        5: light_white,
+        6: light_white,
+        7: light_white,
         8: light_green,
         9: light_green,
         10: light_green
@@ -30,9 +30,9 @@ def colour_map_1_to_10(x):
 def dashed_line(print_out=True, n=50, colour=yellow):
     """Create a dashed line of a prespecified colour."""
     if print_out:
-        print(f"{colour}{n * '-'}{white}")
+        print(f"{colour}{n * '-'}{light_white}")
     else:
-        return colour + (n * "-") + white
+        return colour + (n * "-") + light_white
 
 
 def kv_print(k, v, suffix="", key_width=32, colour_map=False,
@@ -47,7 +47,7 @@ def kv_print(k, v, suffix="", key_width=32, colour_map=False,
         v_val, v_colour = v
     else:
         v_val = v
-        v_colour = white
+        v_colour = light_white
 
     if colour_map:
         v_colour = colour_map_1_to_10(v)
