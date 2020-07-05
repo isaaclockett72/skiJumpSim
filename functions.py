5f43dab43bd4e3e0bd268e4a15bd9a2b80306b30
Ike Lockett <ike.lockett@anaplan.com>
2020-07-05 20:53:49 +0100

    Removed standard_round() since this is now handled by Round objects

--- functions.py
+++ functions.py
@@ -9,10 +9,9 @@ Created on Sat Jun 27 15:18:56 2020
 import pandas as pd
 from pandas import DataFrame
 import random
-from objects import Country, SkiJumper, Jump
-import time
-from console_formatting.formatting import dashed_line, kv_print, line_break
-from console_formatting.ansi_colours import light_green, light_purple, light_white, yellow
+from objects import Country, SkiJumper
+from console_formatting.formatting import kv_print, line_break
+from console_formatting.ansi_colours import light_purple, light_white, yellow
 
 
 def extract_hills(countries):
@@ -69,77 +68,6 @@ def select_country(countries):
     return tournament_country
 
 
-def standard_round(hill, roster, skijumpers, skip_continue_taps=False):
-    """Run a standard round, in order of the roster."""
-    jump_results = []
-    i = 0
-    while True:
-        if i % 10 == 0:
-            skip_ten = False
-        try:
-            current_skijumper = skijumpers[roster.__next__()]
-            i += 1
-        except StopIteration:
-            break
-        print(f"Jumper {i}")
-        current_skijumper.print_stats()
-        if not skip_continue_taps and not skip_ten:
-            user_input = tap_to_continue({"ss": "Skip future taps",
-                                          "s10": "Skip to start of next 10"})
-            skip_continue_taps = user_input == "ss"
-            skip_ten = user_input == "s10"
-        # BASIC RESULTS
-        if jump_results:
-            # kv_print("Current Record", max(results.values()), "m")
-            current_results = DataFrame(jump_results)
-            print(current_results.sort_values("jump_distance",
-                                              ascending=False)[
-                                                  ["skijumper",
-                                                   "jump_distance"]].head(5))
-            line_break()
-        else:
-            kv_print("Calculation line", hill.calculation_line, "m")
-            line_break()
-        # JUMP
-        current_jump = Jump(current_skijumper, hill)
-        current_jump.initiate_jump()
-
-        jump_distance = max(round(current_jump.jump_distance, 2), 0)
-
-        jump_results.append(current_jump.get_series_data())
-        if not skip_continue_taps and not skip_ten:
-            tap_to_continue()
-            time.sleep(0.5)
-        dashed_line(colour=light_green)
-        kv_print("Result", jump_distance, "m")
-        dashed_line(colour=light_green)
-        line_break()
-        dashed_line()
-        if jump_distance > hill.hill_record:
-            print("\nIt's a new hill record!\n")
-            line_break()
-            hill.hill_record = jump_distance
-        results_df = DataFrame(jump_results)
-        print(results_df.sort_values("jump_distance",
-                                     ascending=False)[
-                                    ["skijumper",
-                                     "home_country",
-                                     "jump_distance",
-                                     ]].head(5))
-        results_df.to_pickle(f"results/{hill.name}.pkl")
-        if not skip_continue_taps and not skip_ten:
-            user_input = tap_to_continue({"b": "Breakdown Results"})
-            if user_input == "b":
-                current_jump.print_jump_breakdown()
-                secondary_user_input = tap_to_continue({"ds": "Display Stats"})
-                if secondary_user_input == "ds":
-                    current_skijumper.print_stats()
-                    tap_to_continue()
-
-
-
-
- 
 def tap_to_continue(options=None):
     """Pause until user enters a key."""
     if options:
